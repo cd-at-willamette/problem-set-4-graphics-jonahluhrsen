f1f58d8e70e6304f182724a089c3d5b843fae549
jonahluhrsen <jjluhrsen@willamette.edu>
2024-10-14 16:08:12 -0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -1,7 +1,7 @@
 ########################################
-# Name:
-# Collaborators:
-# Estimated time spent (hrs):
+# Name: Jonah Luhrsen
+# Collaborators: None
+# Estimated time spent (hrs): 2
 ########################################
 
 from pgl import GWindow, GRect
@@ -19,11 +19,25 @@ def draw_pyramid():
 
     gw = GWindow(WIDTH, HEIGHT)
 
-    # You got it from here
+    # i was struggling with this problem and i consulted chatgpt for help
+    # chatgpt was helpful in almost every aspect of this problem and showed me how to correctly determine the starting point
+    # as well as drawing the rows correctly
+    starting_x = (WIDTH - (BRICKS_IN_BASE * BRICK_WIDTH)) / 2
+    starting_y = HEIGHT - BRICK_HEIGHT - 10
 
+    # draw the pyramid row by row
+    for row in range(BRICKS_IN_BASE):
+        bricks_in_row = BRICKS_IN_BASE - row
+        x_center = starting_x + (row * BRICK_WIDTH) / 2
 
+        for brick in range(bricks_in_row):
+            x = x_center + brick * BRICK_WIDTH
+            y = starting_y - row * BRICK_HEIGHT
+            brick_rect = GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT)
+            gw.add(brick_rect)
 
-
+if __name__ == '__main__':
+    draw_pyramid()
 
 
 
