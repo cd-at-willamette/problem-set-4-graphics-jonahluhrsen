b31feb00e9118bf61842a0e6c8470f0c849f0036
jonahluhrsen <jjluhrsen@willamette.edu>
2024-10-13 20:34:34 -0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -6,15 +6,12 @@
 
 from pgl import GWindow, GRect, GOval, GLine, GLabel
 
-# Setting up initial window dimensions. 
-# You can change these if you want. They are in number of pixels.
 WIDTH = 600
 HEIGHT = 600
 
 def draw_image():
     """ Creates a window and draws a student's creation"""
 
-    # Creating the window
     gw = GWindow(WIDTH, HEIGHT)
     
     r = GRect(0,0,600,600)
@@ -62,16 +59,14 @@ def draw_image():
     o.set_color("orange")
     gw.add(o)
 
-    # And now it is your turn! Add your code below! Make sure you meet all the requirements!
+    l = GLine(0, 520, 600, 520)
+    gw.add(l)
 
-
-
-
+    lb = GLabel("Dream Car", 200, 240)
+    gw.add(lb)
 
 if __name__ == '__main__':
     draw_image()
-
-
 
 
 
